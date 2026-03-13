77087fc0d47d6370143506ba666694ad360e30fe
Yaakoub tair <yaakoubt05@gmail.com>
2026-03-13 00:41:32 +0000

    update: add routes for habit logs and stats

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,31 @@
 <?php
 
 use App\Http\Controllers\HabitController;
+use App\Http\Controllers\HabitLogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\StatsController;
 
-Route::get('/me', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('register', [UserController::class, "create"]);
 Route::post('login', [UserController::class, "login"]);
 
-Route::post(
-    "logout",
-    [UserController::class, "logout"]
-)->middleware("auth:sanctum");
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
+    Route::post(
+        "logout",
+        [UserController::class, "logout"]
+    );
+    Route::apiResource("habits", HabitController::class);
+    // Route::apiResource("habitsLog",HabitLogController::class);
+    Route::post("habitsLog", [HabitLogController::class, "store"]);
+    Route::get("habitsLog/{habit_id}", [HabitLogController::class, 'show']);
+    // Route::get('/habits/{id}/stats', [HabitController::class, 'stats']);
+    Route::delete('habits/{id}/logs/{logId}',[HabitLogController::class,'destroy']);
 
-Route::apiResource("habits",HabitController::class)
-->middleware("auth:sanctum");
-
+    Route::get('habits/{id}/stats', [StatsController::class, 'show']);
+    Route::get('stats/overview',    [StatsController::class, 'overview']);
+});
