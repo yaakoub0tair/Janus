e22bb15ffd72c2d6d40ee8cdf03598ea67e25217
Yaakoub tair <yaakoubt05@gmail.com>
2026-03-13 00:34:27 +0000

    feat: create habit_logs table migration

---
+++ database/migrations/2026_03_10_194740_create_habit_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('habit_id')->constrained()->cascadeOnDelete();
+            $table->date('completed_at');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habit_logs');
+    }
+};
